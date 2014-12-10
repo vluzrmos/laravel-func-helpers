87f6198e3c42e9ecd0eb5a0c4edb4ec80c79f7e5
Vagner do Carmo <Vluzrmos@gmail.com>
2014-12-10 01:36:03 -0300

    Update helpers.php

--- src/FuncHelpers/helpers.php
+++ src/FuncHelpers/helpers.php
@@ -14,14 +14,14 @@
  * @return mixed
  */
 function require_helper($helper){
-  return require_once __DIR__."/helpers/{$helper}.php";
+  return require_once __DIR__."/{$helper}.php";
 };
 
 /**
  * Adiciona todos os helpers
  */
 function require_all_helpers(){
-  foreach(glob(__DIR__."/helpers/*.php") as $helper){
+  foreach(glob(__DIR__."/*.php") as $helper){
     require_once($helper);
   }
 }
